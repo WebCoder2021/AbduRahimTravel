a26b0b90901465791b2d383c287f992149ea5940
WebCoder2021 <82173844+WebCoder2021@users.noreply.github.com>
2022-11-24 22:37:03 +0500

    dsfgdf

--- home/models.py
+++ home/models.py
@@ -1,3 +1,74 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser):
+    phone = models.CharField(max_length=200,null=True,blank=True)
+    address = models.CharField(max_length=200,null=True,blank=True)
+    position = models.CharField(max_length=200,null=True,blank=True)
+    image = models.ImageField(upload_to='users',null=True,blank=True)
+
+class Zone(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class CityTown(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='cit-&town/')
+
+    def __str__(self):
+        return self.name
+
+class Region(models.Model):
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='regions/')
+
+    def __str__(self):
+        return self.name
+
+class Convensere(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class WeeklyOffers(models.Model):
+    image = models.ImageField(upload_to='weeklyOffers/')
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    content = models.TextField()
+    peoples = models.IntegerField()
+    phone = models.CharField(max_length=200)
+    email = models.EmailField()
+    address = models.CharField(max_length=200)
+    t_map = models.TextField()
+    convensere = models.ForeignKey(Convensere, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class Country(models.Model):
+    name = models.CharField(max_length=200)
+    population = models.CharField(max_length=200)
+    territory = models.CharField(max_length=200)
+    avg_price = models.CharField(max_length=200)
+    content = models.TextField()
+    city_town = models.ForeignKey(CityTown,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='Country')
+    region = models.ForeignKey(Region,on_delete=models.CASCADE)
+    weekly_offres = models.ForeignKey(WeeklyOffers,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    numbers = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    wekkly_offres = models.ForeignKey(WeeklyOffers,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.user.name + " " + str(self.wekkly_offres.name)
--- travel/settings.py
+++ travel/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'travel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,7 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'travel.wsgi.application'
-
+AUTH_USER_MODEL = 'home.User'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -115,7 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
+
+if DEBUG:
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
